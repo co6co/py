f258f2c9684ca77a11a5f81d4b4d1df51f9538c2
co6co@qq.com <co6co@qq.com>
2023-08-25 13:40:40 +0800

    'app.cl'

--- app/cl/search.py
+++ app/cl/search.py
@@ -5,9 +5,13 @@ class googleSearch:
     def __init__(self):
         self.urlFarmat="https://www.google.com.hk/search?q={0}&sca_esv=559674773&source=hp&ei=gCLnZNzNCIbk2roPqoSR-AM&iflsig=AD69kcEAAAAAZOcwkIGZFfrwft-Ki_Jk76Rd1GOnUaFF&ved=0ahUKEwjcjb-x_fSAAxUGslYBHSpCBD8Q4dUDCAo&uact=5&oq=12&gs_lp=Egdnd3Mtd2l6IgIxMkjXB1DABFiIBXABeACQAQCYAQCgAQCqAQC4AQPIAQD4AQGoAgA&sclient=gws-wiz"
     def search(self,key:str):
-        log.info(self.urlFarmat.format(key))
-        http.get(self.urlFarmat.format(key),30,"127.0.0.1:9666").text
-        
+        try:
+            log.info(self.urlFarmat.format(key))
+            print(http.get(self.urlFarmat.format(key), 30, "127.0.0.1:9666").text)
+        except Exception as e:
+            log.err(e)
+            
 if __name__ == '__main__': 
     gs=googleSearch()
     gs.search("abd")
+    
