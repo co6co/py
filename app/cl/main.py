fa3398ac545189eea11bf6603c87289080ea14c1
co6co@qq.com <co6co@qq.com>
2023-08-25 10:18:05 +0800

    'demo'

---
+++ app/cl/main.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+from co6co.utils import log
+from co6co_clash import nodes
+
---
+++ app/cl/search.py
@@ -0,0 +1,13 @@
+# -*- encoding:utf-8 -*-
+import co6co.utils.http as http
+import co6co.utils.log as log
+class googleSearch:
+    def __init__(self):
+        self.urlFarmat="https://www.google.com.hk/search?q={0}&sca_esv=559674773&source=hp&ei=gCLnZNzNCIbk2roPqoSR-AM&iflsig=AD69kcEAAAAAZOcwkIGZFfrwft-Ki_Jk76Rd1GOnUaFF&ved=0ahUKEwjcjb-x_fSAAxUGslYBHSpCBD8Q4dUDCAo&uact=5&oq=12&gs_lp=Egdnd3Mtd2l6IgIxMkjXB1DABFiIBXABeACQAQCYAQCgAQCqAQC4AQPIAQD4AQGoAgA&sclient=gws-wiz"
+    def search(self,key:str):
+        log.info(self.urlFarmat.format(key))
+        http.get(self.urlFarmat.format(key),30,"127.0.0.1:9666").text
+        
+if __name__ == '__main__': 
+    gs=googleSearch()
+    gs.search("abd")
