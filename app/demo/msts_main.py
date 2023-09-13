9bdcd9f2456fb5a63f6f3943d7a6a26762db2f48
co6co <co6co@qq.com>
2023-09-13 20:12:34 +0800

    'download_args'

--- app/demo/msts_main.py
+++ app/demo/msts_main.py
@@ -5,15 +5,19 @@ import socket,socks
 import re,requests,os,sys
 from urllib.parse import urlparse,unquote_plus,quote_plus,parse_qsl
 import concurrent.futures as futures
-#socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9666)
-#socket.socket = socks.socksocket
+
 from multiprocessing import cpu_count
 import argparse
 
 class msts:
-    def __init__(self,listUrl:str,mp3Url:str="https://177h.wodeshougong.com/",downloadDir="D:\\temp") -> None:
+    @staticmethod
+    def set_proxy(ip:str, port:int):
+        socks.set_default_proxy(socks.SOCKS5, ip, port)
+        socket.socket = socks.socksocket
+
+    def __init__(self,listUrl:str,mp3Url:str,downloadDir="D:\\temp") -> None:
         self.url=listUrl
-        self.mp3host=mp3Url
+        self.mp3host="https://177h.wodeshougong.com/" if mp3Url ==None else mp3Url
         self.downloadFolder=downloadDir
         if not os.path.exists(self.downloadFolder):os.makedirs(self.downloadFolder)
         result=urlparse(listUrl)
@@ -24,8 +28,6 @@ class msts:
         response=http.get(url,header_dict=headers,verify=False)
         response.encoding="utf-8"
         return response.text
-
-    
     def _getList(self)->list|None:
         text=self._get(self.url)
         f=r"/play_m/\d+_\d+_\d+_\d+.html"
@@ -45,8 +47,12 @@ class msts:
             index=r.index('"')
             r=r[index+1:]
             index=r.index('"')
-            r=r[0:index]  
+            r=r[0:index]
             
+            result=urlparse(url)
+            arr=re.findall("\d+",result.path)
+
+            r=r.replace("&id=&ji=",f"&id={arr[0]}&ji=")
             deUrl=unquote_plus(r)
             data=parse_qsl(deUrl)
             mp3Url=data[0][1] 
@@ -59,11 +65,15 @@ class msts:
         fileName=os.path.basename(m)
         header={"Referer":r}
         
-        data=http.download(  requests.utils.requote_uri(m),timeout=30,header_dict=header,verify=False)
-        log.warn(os.path.join(self.downloadFolder,fileName))
-        File=open(os.path.join(self.downloadFolder,fileName),"wb") # Opens the file for writing.
-        File.write(data)
-        log.end_mark("开始下载.")
+        log.info(r+"\r\n"+m)
+        status,data=http.download(requests.utils.requote_uri(m),timeout=30,header_dict=header,verify=False)
+        if status == 200:
+            log.warn(os.path.join(self.downloadFolder,fileName))
+            File=open(os.path.join(self.downloadFolder,fileName),"wb") # Opens the file for writing.
+            File.write(data)
+            log.end_mark("开始下载.")
+        else:
+            log.end_mark(f"下载出错.{status}")
 
     def downloads(self):
         log.start_mark("解析List...")
@@ -81,13 +91,17 @@ if __name__ == '__main__' :
     parser=argparse.ArgumentParser(description="下载文件")
     parser.add_argument("-u","--url",type=str, help="需要下载的URL 列表页")
     parser.add_argument("-m","--murl",type=str, help="媒体主机",default=None)
+    parser.add_argument("-p","--proxy",type=str, help="代理服务器，ip:port",default=None)
     parser.add_argument('--list' ,default=True, action=argparse.BooleanOptionalAction,help=f"Url 是列表页")
     parser.add_argument("-d","--folder",type=str,help=f"保存目录 [{default_save_dir}]",default=default_save_dir)
     args=parser.parse_args()
     c=msts(args.url,mp3Url=args.murl,downloadDir=args.folder)
-    if args.url ==None:
+    if args.proxy !=None:
+        arr = args.proxy.spit(":")
+        msts.set_proxy(arr[0],int(arr[1]))
+    if args.url == None:
         parser.print_help()
         sys.exit(0)
     if args.list: c.downloads()
-    else:  c._download(args.url)
+    else: c._download(args.url)
 
