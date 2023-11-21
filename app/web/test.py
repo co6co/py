f97b41f195fd8cddde8b11dd03f3cc0c3e31644a
co6co <co6co@qq.com>
2023-11-21 22:07:37 +0800

    'x'

--- app/web/test.py
+++ app/web/test.py
@@ -1,7 +1,7 @@
 
 from typing import Tuple,ValuesView,TypeVar,List
 
-def test(a:int ,b:str,*param:str, c:bool=True,**kv:bool):
+def test(a:int ,b:str,c:bool=True,*param:str, **kv:bool):
     print(c)
     print(a,b,c,*param,*kv)
      
