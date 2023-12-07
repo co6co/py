e5cd64f22b654652e5e215c6995c00d7d6d4a4d6
co6co <co6co@qq.com>
2023-12-07 21:41:23 +0800

    'file_hash'

--- app/demo/file_app.py
+++ app/demo/file_app.py
@@ -1,68 +1,7 @@
 import os
-import argparse
-import hashlib
+import argparse 
 
-
-def file_hash(file_path: str, hash_method) -> str:
-    if not os.path.isfile(file_path):
-        print('文件不存在。')
-        return ''
-    h = hash_method()
-    with open(file_path, 'rb') as f:
-        while b := f.read(8192):
-            h.update(b)
-    return h.hexdigest() 
-
-def str_hash(content: str, hash_method, encoding: str = 'UTF-8') -> str:
-    return hash_method(content.encode(encoding)).hexdigest()
-
-
-def file_md5(file_path: str) -> str:
-    return file_hash(file_path, hashlib.md5)
-
-
-def file_sha256(file_path: str) -> str:
-    return file_hash(file_path, hashlib.sha256)
-
-
-def file_sha512(file_path: str) -> str:
-    return file_hash(file_path, hashlib.sha512)
-
-
-def file_sha384(file_path: str) -> str:
-    return file_hash(file_path, hashlib.sha384)
-
-
-def file_sha1(file_path: str) -> str:
-    return file_hash(file_path, hashlib.sha1)
-
-
-def file_sha224(file_path: str) -> str:
-    return file_hash(file_path, hashlib.sha224)
-
-
-def str_md5(content: str, encoding: str = 'UTF-8') -> str:
-    return str_hash(content, hashlib.md5, encoding)
-
-
-def str_sha256(content: str, encoding: str = 'UTF-8') -> str:
-    return str_hash(content, hashlib.sha256, encoding)
-
-
-def str_sha512(content: str, encoding: str = 'UTF-8') -> str:
-    return str_hash(content, hashlib.sha512, encoding)
-
-
-def str_sha384(content: str, encoding: str = 'UTF-8') -> str:
-    return str_hash(content, hashlib.sha384, encoding)
-
-
-def str_sha1(content: str, encoding: str = 'UTF-8') -> str:
-    return str_hash(content, hashlib.sha1, encoding)
-
-
-def str_sha224(content: str, encoding: str = 'UTF-8') -> str:
-    return str_hash(content, hashlib.sha224, encoding) 
+from co6co.utils import hash  
  
 class fileOption:
     filePath:str
@@ -85,21 +24,21 @@ def get_hash_file(folder:str,repeat:bool):
             print("Parent folder:", parent)
             print("Filename:", filename) 
             p=os.path.join(parent,filename)
-            hash=file_md5(p)
-            if repeat: hash_list.append(fileOption(filePath=p,hash=hash)) 
+            h=hash.file_md5(p)
+            if repeat: hash_list.append(fileOption(filePath=p,hash=h)) 
             else: 
                 h_l=[ h.hash for h in hash_list]  
-                if hash not in h_l:hash_list.append(fileOption(filePath=p,hash=hash))  
+                if h not in h_l:hash_list.append(fileOption(filePath=p,hash=h))  
     return hash_list
 
 def main():
     parser=argparse.ArgumentParser(usage="查询MD5")
     parser.add_argument("-d","--folder",default=R"C:\Users\Administrator\Desktop\aw\recovery")
-    parser.add_argument("-c","--repeat", action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument("-r","--repeat", help="dispaly repeat", action=argparse.BooleanOptionalAction, default=False)
     
     args=parser.parse_args()
     hash_list=get_hash_file(args.folder,args.repeat)
-     
+    print(args.repeat)
     for h in hash_list:
         print(h.filePath,'->',h.hash)
 
--- coi/co6co/utils/hash.py
+++ coi/co6co/utils/hash.py
@@ -1,9 +1,65 @@
-import hashlib
+import hashlib,os
 
 def md5(plain_text:str)->str:
     """
     获取MD5摘要
     """
-    md5=hashlib.md5()
-    md5.update(plain_text.encode())
-    return md5.hexdigest()
+    return str_md5(plain_text)
+
+def file_hash(file_path: str, hash_method) -> str:
+    if not os.path.isfile(file_path): raise Exception(message=f"{file_hash} not exist") 
+    h = hash_method()
+    with open(file_path, 'rb') as f:
+        while b := f.read(8192): h.update(b)
+    return h.hexdigest() 
+
+def str_hash(content: str, hash_method, encoding: str = 'UTF-8') -> str:
+    return hash_method(content.encode(encoding)).hexdigest()
+
+
+def file_md5(file_path: str) -> str:
+    return file_hash(file_path, hashlib.md5)
+
+
+def file_sha256(file_path: str) -> str:
+    return file_hash(file_path, hashlib.sha256)
+
+
+def file_sha512(file_path: str) -> str:
+    return file_hash(file_path, hashlib.sha512)
+
+
+def file_sha384(file_path: str) -> str:
+    return file_hash(file_path, hashlib.sha384)
+
+
+def file_sha1(file_path: str) -> str:
+    return file_hash(file_path, hashlib.sha1)
+
+
+def file_sha224(file_path: str) -> str:
+    return file_hash(file_path, hashlib.sha224)
+
+
+def str_md5(content: str, encoding: str = 'UTF-8') -> str:
+    return str_hash(content, hashlib.md5, encoding)
+
+
+def str_sha256(content: str, encoding: str = 'UTF-8') -> str:
+    return str_hash(content, hashlib.sha256, encoding)
+
+
+def str_sha512(content: str, encoding: str = 'UTF-8') -> str:
+    return str_hash(content, hashlib.sha512, encoding)
+
+
+def str_sha384(content: str, encoding: str = 'UTF-8') -> str:
+    return str_hash(content, hashlib.sha384, encoding)
+
+
+def str_sha1(content: str, encoding: str = 'UTF-8') -> str:
+    return str_hash(content, hashlib.sha1, encoding)
+
+
+def str_sha224(content: str, encoding: str = 'UTF-8') -> str:
+    return str_hash(content, hashlib.sha224, encoding) 
