b200d9f60fb1b2e79066b7c8d60be0a428dff19a
co6co <co6co@qq.com>
2023-12-14 22:50:36 +0800

    'result_2'

--- db-ext/co6co_db_ext/res/result.py
+++ db-ext/co6co_db_ext/res/result.py
@@ -1,44 +1,61 @@
 # -*- encoding:utf-8 -*-
 from __future__ import annotations 
-class Result:
-    def __init__(self) -> None:
-        self.code=0
-        self.message=""
-        self.data={} 
-        pass
+from co6co.enums import Base_Enum
+class Result: 
+    def __new__(cls,code:int,data:any,message:str) -> Result:
+        """
+        先执行 __new__ 在执行 __init__
+        在单例模式中 __init__ 会执行多次
+        _instance=None
+        __new__(cls):
+            if cls._instance ==None: cls._instance=object.__new__(cls)
+            return cls._instance 
+        __init__(self):
+            print("init")
+        """
+        obj = object.__new__(cls)
+        obj.code = code 
+        obj.message = message 
+        obj.data=data 
+        return obj  
     
+    code:int
+    message:str
+    data:any
+   
     @staticmethod
     def create(code:int=0,data:any=None,message:str="操作成功")-> Result:
-        result=Result()
-        result.code=code
-        result.data=data
-        result.message=message
-        return result 
+       return Result(code,data ,message ) 
     @staticmethod
     def success(data:any=None,message:str="操作成功")-> Result:
-        return Result.create(data=data,code=0, message=message)
+        return Result.create(data=data,  message=message)
     @staticmethod
     def fail(data:any=None,message:str="处理失败")-> Result:
         return Result.create(data=data,code=500, message=message)
+    
+    def __repr__(self) -> str:
+        return f"class=> <code:{self.code},message:{self.message},data:{self.data}>"
  
 
 class Page_Result(Result):
-    def __init__(self) -> None:
-        super().__init__() 
-        self.total=-1
-
+    total:int
+    def __new__(cls,code:int,data:any,message:str,total:int) -> Page_Result: 
+        obj:Page_Result=super().__new__(cls,code,data,message)
+        obj.total = total  
+        return obj    
     @staticmethod
-    def create(code:int=0,data:any=None,message:str="操作成功")-> Page_Result:
-        result=Page_Result()
-        result.code=code
-        result.data=data
+    def create(code:int=0,data:any=None,message:str="操作成功",total:int=-1)-> Page_Result:
+        result=Page_Result(code=code,data=data,message=message,total=total) 
         return result 
     @staticmethod
-    def success(data:any=None,message:str="操作成功")-> Page_Result:
-         return Result.create(data=data,code=0, message=message)
+    def success(data:any=None,message:str="操作成功",total:int=-1)-> Page_Result:
+         return Page_Result.create(data=data,code=0, message=message,total=total)
     @staticmethod
-    def fail(data:any=None,message:str="处理失败")-> Page_Result:
-        return Result.create(data=data,code=500, message=message)
+    def fail(data:any=None,message:str="处理失败",total:int=-1)-> Page_Result:
+         return Page_Result.create(data=data,code=500, message=message,total=total)
+    
+    def __repr__(self) -> str:
+        return f"class=> <code:{self.code},message:{self.message},total:{self.total},data:{self.data}>"
 
         
 
