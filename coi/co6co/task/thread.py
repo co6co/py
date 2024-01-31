4c8cb11961a5c745c06a887ef171a326cd59935a
co6co <co6co@qq.com>
2024-01-31 22:14:30 +0800

    2

--- coi/co6co/task/thread.py
+++ coi/co6co/task/thread.py
@@ -8,23 +8,23 @@ class ThreadEvent:
 	线程Event loop 
 	Run Event Loop in different thread.
 	
-	# 因某些原因写了该类 
-	# 1. asyncio.run 在 没有正在运行的事件循环 的情况下运行协程的
-    #wx_user_dict:list[dict]=asyncio.run(bll.get_subscribe_alarm_user(config.appid)) 
-    #alarm= alarm_bll(app)
-    #po:bizAlarmTypePO=asyncio.run(alarm.get_alram_type_desc(po.alarmType))
-    # 2. 正在运行的事件循环
-    # This event loop is already running   
-    # import nest_asyncio 
-    #loop = asyncio.get_event_loop()
-    #wx_user_dict:list[dict]=loop.run_until_complete(bll.get_subscribe_alarm_user(config.appid))
-
-    # 3. 创建任务
-    #task=asyncio.create_task(bll.get_subscribe_alarm_user(config.appid))
-    #wx_user_dict:list[dict]=asyncio.run(task) 
-    # 4.底层使用
-    #asyncio.ensure_future(coro())
-    # 5. 
+	## 因某些原因写了该类 
+	## 1. asyncio.run 在 没有正在运行的事件循环 的情况下运行协程的
+    ##wx_user_dict:list[dict]=asyncio.run(bll.get_subscribe_alarm_user(config.appid)) 
+    ##alarm= alarm_bll(app)
+    ##po:bizAlarmTypePO=asyncio.run(alarm.get_alram_type_desc(po.alarmType))
+    ## 2. 正在运行的事件循环
+    ## This event loop is already running   
+    ## import nest_asyncio 
+    ##loop = asyncio.get_event_loop()
+    ##wx_user_dict:list[dict]=loop.run_until_complete(bll.get_subscribe_alarm_user(config.appid)) 
+	
+    ## 3. 创建任务
+    ##task=asyncio.create_task(bll.get_subscribe_alarm_user(config.appid))
+    ##wx_user_dict:list[dict]=asyncio.run(task) 
+    ## 4.底层使用
+    ##asyncio.ensure_future(coro())
+    ## 5. 
 	"""
 	@property
 	def loop(self):
