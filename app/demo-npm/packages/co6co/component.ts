26569b71e1a2b207e0eba5436392f8e2bfda801c
co6co@qq.com <co6co@qq.com>
2024-06-07 17:14:34 +0800

    'demo_test'

--- app/demo-npm/packages/co6co/component.ts
+++ app/demo-npm/packages/co6co/component.ts
@@ -1,7 +1,9 @@
-import { EcDiaglogDetail } from '@co6co/components/tsx'
+//import { EcDiaglogDetail } from '@co6co/components/tsx'
+import { ElAlert } from '@co6co/components/alert'
+import { ElIcon } from '@co6co/components/icon'
 import type { Plugin } from 'vue'
 export default [
-  EcDiaglogDetail,
+  ElAlert,
   /*,
   EcContextMenu,
   EcDetail,
--- app/demo-npm/packages/components/index.ts
+++ app/demo-npm/packages/components/index.ts
@@ -1,1 +1,2 @@
-export * from './tsx' //不知 tsx 组件要怎么导出
+//export * from './tsx' //不知 tsx 组件要怎么导出
+export * from './alert' //不知 tsx 组件要怎么导出
