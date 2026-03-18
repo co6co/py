5e0dbcb91f84a57f04966ace452d673284304ad4
co6co@qq.com <co6co@qq.com>
2026-03-18 17:26:33 +0800

    pwdgen

---
+++ app/cl/tools/cmd.py
@@ -0,0 +1,13 @@
+p="F:\\cygwin64\\bin\\ssh-keygen.exe" 
+import os
+with open("./dist/pwdgen.txt","r") as f:
+    data=f.readlines()
+    for item in data:
+        item=item.strip()
+        # 执行ping命令，但无法获取输出结果
+        exe=p+" -y -f C:\\Users\\Administrator\\.ssh\\id_rsa -P "+item 
+        data=os.system(exe)
+        if data==0:
+            print("成功==>",item)
+            break
+         
--- app/cl/tools/pwdgen.py
+++ app/cl/tools/pwdgen.py
@@ -1,24 +1,48 @@
  
+import argparse
 
 
 
-def getData(p,m,a):
+def jointGenertor(p,m,a):
     for p1 in p:
         for m1 in m:
-            for a1 in a:
+            for a1 in a: 
                 yield p1+m1+a1
-def cc(a1,a2,a3):
+def getData(prefix,middle,append):
+    print("使用字符串：",locals())
     data=[]
-    for item in getData(a1,a2,a3):
+    for item in jointGenertor(prefix,middle,append):
         #if item.startswith("@") or item.startswith("_"):
         #    continue 
         data.append(item)
     return data
-file="./dist/pwdgen.txt"
-with open(file,"w+") as f:
-    for item in data:
-        f.write(item+"\n")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="生成密码")
+    parser.add_argument("-p", "--prefix",  default="admin,abc", type=str, help="密码前缀:admin,abc")
+    parser.add_argument("-m", "--middle",  default="_,@,", type=str, help="密码中间部分:_,@,")
+    parser.add_argument("-a", "--append",  default="123,123456,12345678", type=str, help="密码后缀")
+    parser.add_argument("--append2",  default="", type=str, help="密码后缀2")
+    parser.add_argument("-o", "--output",  default="./dist/pwdgen.txt", type=str, help="输出文件路径")
+    args = parser.parse_args()
+    if not args.prefix and not args.middle and not args.append:
+        parser.print_help()
+        exit(1)
+    file = args.output
+    prefix = args.prefix.split(",")
+    middle = args.middle.split(",")
+    append = args.append.split(",")
+    append2 = args.append2.split(",")
+    data2=getData(prefix,middle,append)
+    
+    if len(append2)>0 and append2[0]!="":
+        data=getData(data2,append2,[''])
+    else:
+        data=data2
+    with open(file,"w+") as f:
+        for item in data:
+            f.write(item+"\n")
+    print("生成密码总数:", len(data))
 
-print("生成密码总数:", len(data))
 
+ 
 
